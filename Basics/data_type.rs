b05f270a2517fcd35463064d18c8b150fc5e1625
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-01-21 21:02:02 +0600

    Create data_type.rs

---
+++ Basics/data_type.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // i32 & i64 is 32 and 64 bit integer data type respectively.
+    let x = 69; // i32
+    // to change the datatype
+    let x:i64 = 69;
+    let x:u64 = 1; // unsigned integers dont support negative numbers
+    let f:f64 = 69.6969; // float
+    let b:bool = true; // boolean
+}
