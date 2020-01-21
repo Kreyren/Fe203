688308dc73b708743ae5447f2cd5e75a4c1e8a32
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-01-21 20:57:28 +0600

    Create mutability.rs

---
+++ Basics/mutability.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // if you want to change the value, use mut. Mutability
+    let mut x = 68;
+    println!("The value of x is: {}", x);
+    x = x+1;
+    println!("The value of x is: {}", x);
+}
