51848e6f23400c12d46298304c2df5eb86d1c480
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-01-21 21:11:03 +0600

    Create if_statements.rs

---
+++ Basics/if_statements.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = 0;
+
+    if x == 3 {
+        println!("Found the number {}", x);
+    } else if x == 4 {
+        println!("Found the other number {}", x);
+    }
+    else {
+        println!("Error");
+    }
+}
