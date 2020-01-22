80650451b4cd2584c161afc6b3e894a2eacc7d43
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-01-22 20:09:22 +0600

    Update tuples.rs

--- Basics/tuples.rs
+++ Basics/tuples.rs
@@ -2,4 +2,8 @@ fn main() {
     let tupl = (69, "test", 69.69, false, (1, 2, 5));
     println!("{}", tupl.1);
     println!("{}", (tupl.4).1);
+    let tupl2 = (69, "test", 69.69);
+    let (a, b, c) = tupl2;
+    println!("A is {}", a);
+    println!("B is {}", b);
 }
