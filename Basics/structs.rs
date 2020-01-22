d4473f5b36b207c86d1c9324438cb3c1ce7f7cd1
Uniminin <42061747+Uniminin@users.noreply.github.com>
2020-01-22 23:51:20 +0600

    Create structs.rs

---
+++ Basics/structs.rs
@@ -0,0 +1,11 @@
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8
+}
+
+fn main() {
+    let mut bg = Color{ red: 255, green: 70, blue: 15};
+    bg.blue = 45;
+    println!("R:{} G:{} B:{}", bg.red, bg.green, bg.blue);
+}
